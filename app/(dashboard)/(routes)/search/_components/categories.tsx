22479aa4660d70848ecd57d830464efe111d61f6
RivixXx <Rivixxx@gmail.com>
2024-02-06 01:14:13 +0300

    Categories Query

---
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import {
+  FcVideoProjector,
+  FcLandscape,
+  FcGlobe,
+  FcPieChart,
+  FcSupport,
+  FcInTransit 
+} from "react-icons/fc";
+import { IconType } from "react-icons";
+
+import { CategoryItem } from "./category-item";
+
+interface CategoriesProps {
+  items: Category[];
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+  "ГлонассСофт": FcGlobe,
+  "1C": FcPieChart,
+  "Wialon": FcInTransit,
+  "Монтаж": FcSupport,
+  "Навигация": FcLandscape,
+  "Тахография": FcVideoProjector,
+};
+
+export const Categories = ({
+  items,
+}: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  )
+}
